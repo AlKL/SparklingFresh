3ed7d9f3147c643f4186b3b78259def16a98cb46
Alex <alexkhien@hotmail.com>
2023-04-30 10:30:08 -0400

    Big overhaul

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,24 @@
-export default function About() {
-    return <h1>About</h1>
+import '../styles/about.css';
+
+const About = () => {
+    return (
+        <>
+            <div className='about sectionWrapper'>
+                <div className='aboutContainer sectionWrapperWidth'>
+                    <div className=''>
+                            Our process
+
+                            At Sparkling Fresh Water, we aim to serve the best quality water possible for our customers.
+                            We have been open operating Sparkling Fresh Water since 2005.
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
+
+export default About;
