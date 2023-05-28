a45dc847a9c96b424ae5d71266a2bc1defb7cb14
Alex <alexkhien@hotmail.com>
2023-05-27 20:29:21 -0400

    Right before padding updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
                     <div className="heroLeftContainer">
                         <div className='topText'><p>Buy bottles and come back for refills!</p></div>
                         <div className='midText'><h1>Stay hydrated with your friendly neighbourhood water supplier.</h1></div>
-                        <div className='btmText'><h4>Local, affordable & water you can trust!</h4></div>
+                        <div className='btmText'><h4>Local and affordable water you can trust!</h4></div>
                     </div>
                     <div className="heroRightContainer">
                         <div className="heroImageContainer">
