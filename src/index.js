052ab068ea0b6d6083d196c627129b6ecb8457f9
Alex <alexkhien@hotmail.com>
2023-06-05 18:52:16 -0400

    Remove basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <HashRouter basename="/alkl.github.io">
+    <HashRouter>
       <App />
     </HashRouter>
   // </React.StrictMode>
